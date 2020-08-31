211ef9995cac7ee20273b5f75d20297b198e77a8
plusbeauxjours <plusbeauxjours@gmail.com>
2020-08-31 18:42:34 +0700

    SET project renaming

--- android/app/src/main/java/com/cuwesop/MainActivity.java
+++ android/app/src/main/java/com/cuwesop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.shopsol;
+package com.cuwesop;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Shopsol";
+    return "CUwesop";
   }
 }
--- ios/CUwesopTests/CUwesopTests.m
+++ ios/CUwesopTests/CUwesopTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ShopsolTests : XCTestCase
+@interface CUwesopTests : XCTestCase
 
 @end
 
-@implementation ShopsolTests
+@implementation CUwesopTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
